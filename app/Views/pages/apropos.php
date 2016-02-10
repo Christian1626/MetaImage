d7ab645cf813e604ffa8b4d15e7c087a6426144f
Christian1626 <leroy_christian@hotmail.fr>
2016-02-10 17:31:25 +0100

    insertion base d'images

--- app/Views/pages/apropos.php
+++ app/Views/pages/apropos.php
@@ -63,7 +63,7 @@
 	Nous utilisons des appels à exiftool uniquement pour créer les fichiers JSON qui servent au site mais également lorsqu'on modifie les métadonnées d'une image, dans ce cas le fichier JSON est recréé.</br>
 	C'est donc avec ce système que nous avons 'optimisé' le site, en ne réduisant les appels à exiftool qu'en cas d'upload et de modification d'image, ne prenant ensuite que les fichiers JSON pour les différents affichages.</br>
 
-	<h3>Modification de différents champs 'identiques'</h3>
+	<h3>Modification des metadata d'une image</h3>
 	Dans les métadonnées il existe des champs parlant des mêmes valeurs, par exemple "object name", "title" et "headline". Afin de garder une cohérence dans les données, nous avons mit en place un algorithme qui lie chaque champ modifié dans l'application à un ensemble de champ des métadonnées et qui modifient les champs seulement si ceux-ci sont présents dans le .json.</br>
 	L'algorithme, lors de la modification, liste tout les champs des métadonnées ayant exactement la même valeur que la valeur précédente du champ qu'on veut modifier, les stocke, et les modifie ensuite tous avec la nouvelle valeur.</br>
 	Ainsi, au lieu de mettre en place un formulaire supplémentaire nécessitant l'action d'un utilisateur pour valider le problème, nous avons automatisé la modification pour la rendre transparente à l'utilisateur.</br>
